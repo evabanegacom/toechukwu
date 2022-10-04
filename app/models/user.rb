3c08123ea2189209a4c5c47218dd0daae68ec8bd
evabanegacom <udegbue69@gmail.com>
2022-10-04 10:19:55 +0100

    fixed dupplicte records

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     scope :unique_by_category, lambda { select('DISTINCT(category)') }
 
     validates :first_name, presence: true
+    validates_uniqueness_of :first_name, scope: :last_name
     validates :last_name, presence: true
     validates :email, presence: true
     validates :phone, presence: true
